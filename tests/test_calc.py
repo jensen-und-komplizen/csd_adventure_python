83de77adeadc6391d2bb47f4ebb7b56de679ae2f
Björn Jensen <mirouhh@googlemail.com>
2023-05-14 17:28:12 +0200

    Adding missing files in order to be able to execute all tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -8,8 +8,3 @@ class TestCalc(unittest.TestCase):
 	def test_add(self):
 		result = add(1, 2)
 		assert result == 3
-
-
-if __name__ == '__main__':
-	unittest.main()
-	
--- tests/test_loo/test_colleagues/test_pizza_the_hut.py
+++ tests/test_loo/test_colleagues/test_pizza_the_hut.py
@@ -8,7 +8,3 @@ class TestPizzaTheHut(unittest.TestCase):
     def test_greet(self):
         pizza_the_hut = PizzaTheHut()
         assert pizza_the_hut.greet() == "Pizza The Hut is greeting you friendly: Hello dear friend! Want to have a slice of my pizza?"
-
-
-if __name__ == '__main__':
-    unittest.main()
