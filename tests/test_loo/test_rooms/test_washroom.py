832c709409def25815e8928568a50af407cdfa85
Max Lütkemeyer <Max.Luetkemeyer@outlook.de>
2023-05-16 11:42:27 +0200

    highlights on useable items

--- tests/test_loo/test_rooms/test_washroom.py
+++ tests/test_loo/test_rooms/test_washroom.py
@@ -7,4 +7,4 @@ class TestWashroom(unittest.TestCase):
 
     def test_greet(self):
         washroom = WashRoom()
-        assert washroom.get_detailed_description() == "You see an incredibly nasty sink with an undefinable substance in it. Ew!" + "<br/>" + "You notice a DoD on the door." + "</br>" + "On the other side of the room you see another door." + "</br>" + "Oh, and there's a blockchain in the corner. Interesting.."
+        assert washroom.get_detailed_description() == "You see a coin on the floor. " + "<br/>" + "You also see an incredibly nasty sink with an undefinable substance in it. Ew!" + "<br/>" + "You notice a <mark>DoD</mark> on the door." + "</br>" + "On the other side of the room you see another <mark>door</mark>." + "</br>" + "Oh, and there's a blockchain in the corner. Interesting.."
