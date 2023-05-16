3735d4e38cb03151bea3a9654327a82fbbfa027b
Michael Maetzig <mmaetzig@deloitte.de>
2023-05-16 12:06:56 +0200

    Added "grab blockchain" command incl. test function

--- tests/test_loo/test_rooms/test_washroom.py
+++ tests/test_loo/test_rooms/test_washroom.py
@@ -8,3 +8,7 @@ class TestWashroom(unittest.TestCase):
     def test_greet(self):
         washroom = WashRoom()
         assert washroom.get_detailed_description() == "You see an incredibly nasty sink with an undefinable substance in it. Ew!" + "<br/>" + "You notice a DoD on the door." + "</br>" + "On the other side of the room you see another door." + "</br>" + "Oh, and there's a blockchain in the corner. Interesting.."
+
+    def test_blockchain(self):
+        washroom = WashRoom()
+        assert washroom.handle_command(self, "grab blockchain") == "Nice, now I have a blockhain in my pocket. Maybe I will become a Crpyto millionaire?!"
