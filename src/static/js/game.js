7fbf445526a22684c62eef429be3c2d7377334e6
Dominic Schuld <schuld@next-kraftwerke.de>
2024-04-17 11:41:58 +0200

    Add console like behaviour for text commands in frontend

--- src/static/js/game.js
+++ src/static/js/game.js
@@ -1,7 +1,13 @@
+let commandArray = [];
+lastItemIndex = 0
+searchIndex = 1
+
+// Push items to the array
 $( document ).ready(function() {
     $('#command').focus();
     $('#commandForm').submit(function( event ) {
         event.preventDefault();
+        updateCommandArray($('#command').val());
         $.ajax({
             url: "/command",
             data: {command: $('#command').val()},
@@ -15,3 +21,27 @@ $( document ).ready(function() {
             });
     });
 });
+
+// Listen to keystrokes
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 38: // up arrow
+            if (searchIndex < commandArray.length) {
+                searchIndex++
+            }
+            $('#command').val(commandArray[lastItemIndex - searchIndex]);
+            break;
+        case 40: // down arrow
+            if (searchIndex > 0) {
+                searchIndex--
+            }
+            $('#command').val(commandArray[lastItemIndex - searchIndex]);
+            break;
+    }
+});
+
+function updateCommandArray(command){
+    commandArray.push(command);
+    lastItemIndex = commandArray.length
+    searchIndex = 0
+}
