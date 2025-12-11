f6bae9c31b9229aff9453604ed09fb6308d57a7d
wintersections <94918309+wintersections@users.noreply.github.com>
2025-12-11 16:13:02 +0100

    bug/text-area-flicker (#9)
    * Send command directly from button
    
    instead of going through text area, form-submitting and consequently
    causing flickering in the text area.
    
    * Refactor code repetition by introducing submitCommand() function
    
    ---------
    
    Co-authored-by: Julian Theis <theis@next-kraftwerke.de>

--- src/static/js/game.js
+++ src/static/js/game.js
@@ -7,23 +7,27 @@ $( document ).ready(function() {
     $('#command').focus();
     $('#commandForm').submit(function( event ) {
         event.preventDefault();
-        updateCommandArray($('#command').val());
-        $.ajax({
-            url: "/command",
-            data: {command: $('#command').val()},
-        })
-            .done(function( data ) {
-                if ( console && console.log ) {
-                    console.log( "Sample of data--:", data.slice( 0, 100 ) );
-                }
-                $("#gameInfo").html(data);
-                $('#command').val("");
-            });
+        submitCommand($('#command').val());
     });
     $('.standard-command-button').on('click', ({ currentTarget }) => {
-        $('#command').val(currentTarget.innerText).trigger('submit');
+        submitCommand(currentTarget.innerText);
     });
 });
+
+const submitCommand = (content) => {
+    updateCommandArray(content);
+    $.ajax({
+        url: "/command",
+        data: {command: content},
+    })
+        .done(function( data ) {
+            if ( console && console.log ) {
+                console.log( "Sample of data--:", data.slice( 0, 100 ) );
+            }
+            $("#gameInfo").html(data);
+            $('#command').val("");
+        });
+}
 
 // Listen to keystrokes
 $(document).keydown(function(e) {
