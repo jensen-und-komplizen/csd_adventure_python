a76cfaf9619c011ebf11917519abf1fd350ed749
Matthew Frazier <frazier@next-kraftwerke.de>
2025-12-11 16:01:00 +0100

    remove unused Clock and Timeservice to fix CI

--- tests/test_loo/test_items/test_clock.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from src.loo.items.Timeservice import TimeService
-
-class TestTimeService(unittest.TestCase):
-    def test_current_time_looks_good(self):
-        timestamp = TimeService.current_time()
-        self.assertGreater(timestamp, -1, "timestamp should be greater than -1")
-
